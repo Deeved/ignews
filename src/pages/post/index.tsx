76a5d7eb2d03e7bccaf20444238c5e2187812695
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-02-03 14:51:06 -0300

    Consuming API Prismic

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,38 +1,72 @@
-import Head from 'next/head'
+import { GetStaticProps } from "next";
+import Head from "next/head";
 
-import styles  from './style.module.scss'
+import Prismic from "@prismicio/client";
+import { getPrismicClient } from "../../services/prismic";
+
+import styles from "./style.module.scss";
 
 export default function Post() {
-    return (
-        <>
-            <Head>
-                <title>Post | Ignews</title>
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>Post | Ignews</title>
+      </Head>
 
-            <main className={styles.container}>
-                <div className={styles.posts}>
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multple packages with a shared build, test and release process.</p>
-                    </a> 
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multple packages with a shared build, test and release process.</p>
-                    </a> 
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multple packages with a shared build, test and release process.</p>
-                    </a> 
-                    <a href='#'>
-                        <time>12 de março de 2021</time>
-                        <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
-                        <p>In this guide, you will learn how to create a Monorepo to manage multple packages with a shared build, test and release process.</p>
-                    </a> 
-                </div>
-            </main>
-        </>
-    )
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multple packages with a shared build, test and release process.
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multple packages with a shared build, test and release process.
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multple packages with a shared build, test and release process.
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepa with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multple packages with a shared build, test and release process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at("document.type", "post")],
+    {
+      fetch: ["post.title", "post.content"],
+      pageSize: 100,
+    }
+  );
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {},
+  };
+};

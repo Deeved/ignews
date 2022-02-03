2636881ad9d05c842e35a24f8b9b235d68ab825e
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-02-03 16:21:14 -0300

    Menu navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
+
 import { SignInButton } from "../SignInButton";
 import styles from "./style.module.scss";
-
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Post</a>
+          <Link href="/home">
+            <a className={styles.active}>Home</a>
+          </Link>
+
+          <Link href="/post">
+            <a>Post</a>
+          </Link>
         </nav>
 
         <SignInButton />
